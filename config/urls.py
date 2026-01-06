4affc15d821dff79dbc52ead0dca9a6fcd29adc5
kimjinsu <creeeeeeeeeeeeeping@gmail.com>
2026-01-06 16:41:25 +0900

    Resolve conflicts after rebase

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
