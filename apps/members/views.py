e21808cba623929af2daf32d2c5993ee13d94418
jhjdl3984 <jhjdl3984@gmail.com>
2026-01-07 17:36:26 +0900

    🐛 fix: ruff 오류 해결

--- apps/members/views.py
+++ apps/members/views.py
@@ -24,42 +24,6 @@ class UserSignupView(GenericAPIView):
         return Response(response_data, status=status.HTTP_201_CREATED)
 
 # 로그인 view
-# class UserLoginView(APIView):
-#     def post(self, request):
-#         serializer = UserLoginRequestSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-
-#         email = serializer.validated_data["email"]
-#         password = serializer.validated_data["password"]
-
-#         # 로그인 인증
-#         user = authenticate(request, email=email, password=password)
-#         if not user:
-#             return Response({"detail": "잘못된 자격 증명"}, status=status.HTTP_401_UNAUTHORIZED)
-
-#         # JWT 생성
-#         refresh = RefreshToken.for_user(user)
-#         access_token = str(refresh.access_token)
-
-#         # Nested Serializer로 user 데이터 포함
-#         response_data = {
-#             "user": UserSignupResponseSerializer(user).data,
-#             "token": access_token
-#         }
-
-#         # 응답 + Refresh Token Cookie 설정
-#         response = Response(response_data, status=status.HTTP_200_OK)
-#         response.set_cookie(
-#             key="refresh_token",
-#             value=str(refresh),
-#             httponly=True,
-#             secure=True,   # HTTPS 환경
-#             samesite="Lax",
-#             max_age=7*24*60*60  # 7일
-#         )
-
-#         return response
-
 class UserLoginView(GenericAPIView):
     serializer_class = UserLoginRequestSerializer
 
