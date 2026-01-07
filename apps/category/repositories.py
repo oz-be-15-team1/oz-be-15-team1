52785fdf4d255439e025c3715daae00615bed193
kimjinsu <creeeeeeeeeeeeeping@gmail.com>
2026-01-07 15:47:07 +0900

    💡 chore : 코드 추가 ( category, tag )

--- apps/category/repositories.py
+++ apps/category/repositories.py
@@ -1,4 +1,5 @@
 from django.db.models import QuerySet
+
 from .models import Category
 
 
--- apps/tag/urls.py
+++ apps/tag/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from .views import (
-    TagListCreateView,
     TagDetailView,
-    TagTrashListView,
+    TagListCreateView,
     TagRestoreView,
+    TagTrashListView,
 )
 
 app_name = "tag"
