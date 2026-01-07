a52c8842a0a796561a8873c13e5e107d66d1c5b3
jhjdl3984 <jhjdl3984@gmail.com>
2026-01-07 17:29:47 +0900

    🐛 fix: ruff 오류 해결

--- apps/members/views.py
+++ apps/members/views.py
@@ -1,9 +1,7 @@
-from rest_framework.generics import GenericAPIView
 from django.contrib.auth import authenticate
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-
-from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import (
