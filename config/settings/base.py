949eec7cfb2ea669ddc3810f4e275b23d4da2713
kimjinsu <creeeeeeeeeeeeeping@gmail.com>
2026-01-13 13:50:11 +0900

    🐛 fix : 기능 수정, 버그 수정 (배포 전 변경사항)

--- config/settings/base.py
+++ config/settings/base.py
@@ -197,3 +197,19 @@ CELERY_TIMEZONE = TIME_ZONE
 
 # bubget
 BUDGET_ALERT_DEDUP_MINUTES = 5
+
+# database
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("DB_NAME", "django"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    }
+}
